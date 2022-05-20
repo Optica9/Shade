3aebb8395dd3f6d9bd45b6dbafce2aa4c057b373
Rival <81734544+ryvl@users.noreply.github.com>
2022-05-19 22:49:24 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 9474050899 then -- Jackpot Party
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/ryvl/Shade/main/Jackpot%20Party", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ryvl/Shade/main/Jackpot%20Party.lua", true))()
 end
